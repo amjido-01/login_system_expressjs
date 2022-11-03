65adf782755a143306a81a46230e9dc21f54161b
alajido <96017433+Alajido@users.noreply.github.com>
2022-11-03 17:18:07 +0100

    commit

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,10 @@ router.get('/', (req, res) => {
     res.render('welcome');
 })
 
-// router.get('/dashboard', ensureAuthenticated, (req,res)=>{
-//     res.render('dashboard', {
-//         name: req.user.name
-//     });
-// })
+
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
     res.render('dashboard', {
-        name: req.user.name
+        user: req.user
     })
 })
 
